efe6b2921a803bb4b1cf79ea6e6c11eabb32b00e
PauloRX <paulo.xavier.smart@gmail.com>
2023-01-16 23:29:10 -0300

    Initial Commit

---
+++ src/main/java/br/com/paulorx/alurafloricultura/AluraFloriculturaApplication.java
@@ -0,0 +1,13 @@
+package br.com.paulorx.alurafloricultura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluraFloriculturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AluraFloriculturaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/paulorx/alurafloricultura/AluraFloriculturaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.paulorx.alurafloricultura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AluraFloriculturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
